286755f62a0e8dd29f7b07c4f6f2126c09ad2664
gabberyman <128709967+gabberyman@users.noreply.github.com>
2025-11-13 12:29:49 +0100

    SQL file added

---
+++ conceptual_model.sql
@@ -0,0 +1,123 @@
+CREATE TABLE course_layout (
+ course_code CHAR(6) NOT NULL,
+ course_name VARCHAR(2000) NOT NULL,
+ min_students INT,
+ max_students INT,
+ hp FLOAT(2)
+);
+
+ALTER TABLE course_layout ADD CONSTRAINT PK_course_layout PRIMARY KEY (course_code);
+
+
+CREATE TABLE department (
+ department_name VARCHAR(2000) NOT NULL,
+ manager VARCHAR(2000)
+);
+
+ALTER TABLE department ADD CONSTRAINT PK_department PRIMARY KEY (department_name);
+
+
+CREATE TABLE job_title (
+ job_title VARCHAR(20) NOT NULL
+);
+
+ALTER TABLE job_title ADD CONSTRAINT PK_job_title PRIMARY KEY (job_title);
+
+
+CREATE TABLE person (
+ personal_number CHAR(12) NOT NULL,
+ first_name CHAR(10),
+ last_name CHAR(10),
+ address CHAR(10)
+);
+
+ALTER TABLE person ADD CONSTRAINT PK_person PRIMARY KEY (personal_number);
+
+
+CREATE TABLE phone_number (
+ phone_nr INT NOT NULL,
+ personal_number CHAR(12) NOT NULL
+);
+
+ALTER TABLE phone_number ADD CONSTRAINT PK_phone_number PRIMARY KEY (phone_nr);
+
+
+CREATE TABLE teaching_activity (
+ activity_name VARCHAR(20) NOT NULL,
+ factor FLOAT(2)
+);
+
+ALTER TABLE teaching_activity ADD CONSTRAINT PK_teaching_activity PRIMARY KEY (activity_name);
+
+
+CREATE TABLE course_instance (
+ instance_id CHAR(10) NOT NULL,
+ num_students INT,
+ study_period CHAR(2),
+ study_year INT,
+ course_code CHAR(6) NOT NULL
+);
+
+ALTER TABLE course_instance ADD CONSTRAINT PK_course_instance PRIMARY KEY (instance_id);
+
+
+CREATE TABLE employee (
+ employment_id CHAR(10) NOT NULL,
+ salary CHAR(10),
+ supervisor/manager CHAR(10),
+ department_name VARCHAR(2000) NOT NULL,
+ personal_number CHAR(12) NOT NULL,
+ job_title VARCHAR(20) NOT NULL
+);
+
+ALTER TABLE employee ADD CONSTRAINT PK_employee PRIMARY KEY (employment_id);
+
+
+CREATE TABLE planned_activity (
+ activity_id INT NOT NULL,
+ planned_hours INT,
+ instance_id CHAR(10) NOT NULL,
+ activity_name VARCHAR(20) NOT NULL
+);
+
+ALTER TABLE planned_activity ADD CONSTRAINT PK_planned_activity PRIMARY KEY (activity_id);
+
+
+CREATE TABLE skill_set (
+ skill VARCHAR(2000) NOT NULL,
+ employment_id CHAR(10) NOT NULL
+);
+
+ALTER TABLE skill_set ADD CONSTRAINT PK_skill_set PRIMARY KEY (skill,employment_id);
+
+
+CREATE TABLE employee_activity (
+ activity_id INT NOT NULL,
+ employment_id CHAR(10) NOT NULL
+);
+
+ALTER TABLE employee_activity ADD CONSTRAINT PK_employee_activity PRIMARY KEY (activity_id,employment_id);
+
+
+ALTER TABLE phone_number ADD CONSTRAINT FK_phone_number_0 FOREIGN KEY (personal_number) REFERENCES person (personal_number);
+
+
+ALTER TABLE course_instance ADD CONSTRAINT FK_course_instance_0 FOREIGN KEY (course_code) REFERENCES course_layout (course_code);
+
+
+ALTER TABLE employee ADD CONSTRAINT FK_employee_0 FOREIGN KEY (department_name) REFERENCES department (department_name);
+ALTER TABLE employee ADD CONSTRAINT FK_employee_1 FOREIGN KEY (personal_number) REFERENCES person (personal_number);
+ALTER TABLE employee ADD CONSTRAINT FK_employee_2 FOREIGN KEY (job_title) REFERENCES job_title (job_title);
+
+
+ALTER TABLE planned_activity ADD CONSTRAINT FK_planned_activity_0 FOREIGN KEY (instance_id) REFERENCES course_instance (instance_id);
+ALTER TABLE planned_activity ADD CONSTRAINT FK_planned_activity_1 FOREIGN KEY (activity_name) REFERENCES teaching_activity (activity_name);
+
+
+ALTER TABLE skill_set ADD CONSTRAINT FK_skill_set_0 FOREIGN KEY (employment_id) REFERENCES employee (employment_id);
+
+
+ALTER TABLE employee_activity ADD CONSTRAINT FK_employee_activity_0 FOREIGN KEY (activity_id) REFERENCES planned_activity (activity_id);
+ALTER TABLE employee_activity ADD CONSTRAINT FK_employee_activity_1 FOREIGN KEY (employment_id) REFERENCES employee (employment_id);
+
+
