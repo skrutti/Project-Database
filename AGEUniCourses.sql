c42c776b2ace7be56e2ab89c3da9715cd4c6b24f
skrutti <63068404+skrutti@users.noreply.github.com>
2025-11-25 11:40:57 +0100

    Update AGEUniCourses.sql

--- AGEUniCourses.sql
+++ AGEUniCourses.sql
@@ -1,7 +1,18 @@
 DROP TABLE IF EXISTS course_layout CASCADE;
+DROP TABLE IF EXISTS department CASCADE;
+DROP TABLE IF EXISTS job_title CASCADE;
+DROP TABLE IF EXISTS person CASCADE;
+DROP TABLE IF EXISTS phone_number CASCADE;
+DROP TABLE IF EXISTS study_period CASCADE;
+DROP TABLE IF EXISTS teaching_activity CASCADE;
+DROP TABLE IF EXISTS course_instance CASCADE;
+DROP TABLE IF EXISTS employee CASCADE;
+DROP TABLE IF EXISTS planned_activity CASCADE;
+DROP TABLE IF EXISTS skill_set CASCADE;
+DROP TABLE IF EXISTS employee_activity CASCADE;
 
 CREATE TABLE course_layout (
- course_id SERIAL,
+ course_id  INT NOT NULL,
  course_code CHAR(6) NOT NULL,
  course_name VARCHAR(2000) NOT NULL,
  min_students INT,
@@ -9,26 +20,26 @@ CREATE TABLE course_layout (
  hp FLOAT
 );
 
-ALTER TABLE course_layout ADD CONSTRAINT PK_course_layout PRIMARY KEY (course_id);
+ALTER TABLE course_layout ADD CONSTRAINT PK_course_layout PRIMARY KEY (course_id );
 
-DROP TABLE IF EXISTS department CASCADE;
 
 CREATE TABLE department (
+ department_id INT NOT NULL,
  department_name VARCHAR(2000) NOT NULL,
  manager CHAR(10)
 );
 
-ALTER TABLE department ADD CONSTRAINT PK_department PRIMARY KEY (department_name);
+ALTER TABLE department ADD CONSTRAINT PK_department PRIMARY KEY (department_id);
 
-DROP TABLE IF EXISTS job_title CASCADE;
 
 CREATE TABLE job_title (
+ job_title_id INT NOT NULL,
  job_title VARCHAR(20) NOT NULL
 );
 
-ALTER TABLE job_title ADD CONSTRAINT PK_job_title PRIMARY KEY (job_title);
+ALTER TABLE job_title ADD CONSTRAINT PK_job_title PRIMARY KEY (job_title_id);
 
-DROP TABLE IF EXISTS person CASCADE;
+
 CREATE TABLE person (
  personal_number CHAR(12) NOT NULL,
  first_name VARCHAR(50),
@@ -38,7 +49,7 @@ CREATE TABLE person (
 
 ALTER TABLE person ADD CONSTRAINT PK_person PRIMARY KEY (personal_number);
 
-DROP TABLE IF EXISTS phone_number CASCADE;
+
 CREATE TABLE phone_number (
  phone_nr INT NOT NULL,
  personal_number CHAR(12) NOT NULL
@@ -47,52 +58,55 @@ CREATE TABLE phone_number (
 ALTER TABLE phone_number ADD CONSTRAINT PK_phone_number PRIMARY KEY (phone_nr);
 
 
-DROP TABLE IF EXISTS teaching_activity CASCADE;
+CREATE TABLE study_period (
+ study_period CHAR(2) NOT NULL
+);
+
+ALTER TABLE study_period ADD CONSTRAINT PK_study_period PRIMARY KEY (study_period);
+
+
 CREATE TABLE teaching_activity (
+ teaching_id INT NOT NULL,
  activity_name VARCHAR(20) NOT NULL,
  factor FLOAT(2)
 );
 
-ALTER TABLE teaching_activity ADD CONSTRAINT PK_teaching_activity PRIMARY KEY (activity_name);
+ALTER TABLE teaching_activity ADD CONSTRAINT PK_teaching_activity PRIMARY KEY (teaching_id);
 
 
-DROP TABLE IF EXISTS course_instance CASCADE;
 CREATE TABLE course_instance (
  instance_id CHAR(10) NOT NULL,
  num_students INT,
- study_period CHAR(2),
  study_year INT,
- course_id INT NOT NULL
+ course_id  INT NOT NULL,
+ study_period CHAR(2) NOT NULL
 );
 
 ALTER TABLE course_instance ADD CONSTRAINT PK_course_instance PRIMARY KEY (instance_id);
 
 
-DROP TABLE IF EXISTS employee CASCADE;
 CREATE TABLE employee (
  employment_id CHAR(10) NOT NULL,
  salary INT,
  supervisor CHAR(10),
- department_name VARCHAR(2000) NOT NULL,
  personal_number CHAR(12) NOT NULL,
- job_title VARCHAR(20) NOT NULL
+ department_id INT NOT NULL,
+ job_title_id INT NOT NULL
 );
 
 ALTER TABLE employee ADD CONSTRAINT PK_employee PRIMARY KEY (employment_id);
 
 
-DROP TABLE IF EXISTS planned_activity CASCADE;
 CREATE TABLE planned_activity (
  activity_id INT NOT NULL,
- planned_hours INT,
  instance_id CHAR(10) NOT NULL,
- activity_name VARCHAR(20) NOT NULL
+ planned_hours INT,
+ teaching_id INT NOT NULL
 );
 
-ALTER TABLE planned_activity ADD CONSTRAINT PK_planned_activity PRIMARY KEY (activity_id);
+ALTER TABLE planned_activity ADD CONSTRAINT PK_planned_activity PRIMARY KEY (activity_id,instance_id);
 
 
-DROP TABLE IF EXISTS skill_set CASCADE;
 CREATE TABLE skill_set (
  skill VARCHAR(50) NOT NULL,
  employment_id CHAR(10) NOT NULL
@@ -101,38 +115,35 @@ CREATE TABLE skill_set (
 ALTER TABLE skill_set ADD CONSTRAINT PK_skill_set PRIMARY KEY (skill,employment_id);
 
 
-DROP TABLE IF EXISTS employee_activity CASCADE;
 CREATE TABLE employee_activity (
+ employment_id CHAR(10) NOT NULL,
  activity_id INT NOT NULL,
- employment_id CHAR(10) NOT NULL
+ instance_id CHAR(10) NOT NULL
 );
 
-ALTER TABLE employee_activity ADD CONSTRAINT PK_employee_activity PRIMARY KEY (activity_id,employment_id);
+ALTER TABLE employee_activity ADD CONSTRAINT PK_employee_activity PRIMARY KEY (employment_id,activity_id,instance_id);
 
 
 ALTER TABLE phone_number ADD CONSTRAINT FK_phone_number_0 FOREIGN KEY (personal_number) REFERENCES person (personal_number);
 
 
-ALTER TABLE course_instance ADD CONSTRAINT FK_course_instance_0 FOREIGN KEY (course_id) REFERENCES course_layout (course_id);
+ALTER TABLE course_instance ADD CONSTRAINT FK_course_instance_0 FOREIGN KEY (course_id ) REFERENCES course_layout (course_id );
+ALTER TABLE course_instance ADD CONSTRAINT FK_course_instance_1 FOREIGN KEY (study_period) REFERENCES study_period (study_period);
 
 
-ALTER TABLE employee ADD CONSTRAINT FK_employee_0 FOREIGN KEY (department_name) REFERENCES department (department_name);
-ALTER TABLE employee ADD CONSTRAINT FK_employee_1 FOREIGN KEY (personal_number) REFERENCES person (personal_number);
-ALTER TABLE employee ADD CONSTRAINT FK_employee_2 FOREIGN KEY (job_title) REFERENCES job_title (job_title);
+ALTER TABLE employee ADD CONSTRAINT FK_employee_0 FOREIGN KEY (personal_number) REFERENCES person (personal_number);
+ALTER TABLE employee ADD CONSTRAINT FK_employee_1 FOREIGN KEY (department_id) REFERENCES department (department_id);
+ALTER TABLE employee ADD CONSTRAINT FK_employee_2 FOREIGN KEY (job_title_id) REFERENCES job_title (job_title_id);
 
 
 ALTER TABLE planned_activity ADD CONSTRAINT FK_planned_activity_0 FOREIGN KEY (instance_id) REFERENCES course_instance (instance_id);
-ALTER TABLE planned_activity ADD CONSTRAINT FK_planned_activity_1 FOREIGN KEY (activity_name) REFERENCES teaching_activity (activity_name);
+ALTER TABLE planned_activity ADD CONSTRAINT FK_planned_activity_1 FOREIGN KEY (teaching_id) REFERENCES teaching_activity (teaching_id);
 
 
 ALTER TABLE skill_set ADD CONSTRAINT FK_skill_set_0 FOREIGN KEY (employment_id) REFERENCES employee (employment_id);
 
 
-ALTER TABLE employee_activity ADD CONSTRAINT FK_employee_activity_0 FOREIGN KEY (activity_id) REFERENCES planned_activity (activity_id);
-ALTER TABLE employee_activity ADD CONSTRAINT FK_employee_activity_1 FOREIGN KEY (employment_id) REFERENCES employee (employment_id);
-
-
---DATA should be moved to second file
-  -- ('XD2447','erat neque',31,96,'7.5'),
+ALTER TABLE employee_activity ADD CONSTRAINT FK_employee_activity_0 FOREIGN KEY (employment_id) REFERENCES employee (employment_id);
+ALTER TABLE employee_activity ADD CONSTRAINT FK_employee_activity_1 FOREIGN KEY (activity_id,instance_id) REFERENCES planned_activity (activity_id,instance_id);
 
 
