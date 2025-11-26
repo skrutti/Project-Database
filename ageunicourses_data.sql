e77fc6a2438013e79a07dc8a44a33753f9ded8ad
gabberyman <128709967+gabberyman@users.noreply.github.com>
2025-11-26 12:35:55 +0100

    Update ageunicourses_data.sql

--- ageunicourses_data.sql
+++ ageunicourses_data.sql
@@ -276,4 +276,8 @@ INSERT INTO employee_activity (employment_id, activity_id, instance_id) VALUES
 ('EMP009', 60, '2025-99013'),  -- TA for Software Testing Lecture
 ('EMP009', 61, '2025-99013'),  -- TA for Software Testing Tutorial
 ('EMP010', 62, '2025-10124'),  -- TA for Project Management Lecture
+<<<<<<< Updated upstream
 ('EMP010', 63, '2025-10124');  -- TA for Project Management Seminar
+=======
+('EMP010', 63, '2025-10124');  -- TA for Project Management Seminar
+>>>>>>> Stashed changes
